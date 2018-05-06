251ddb90f2b36d4622837d8b52321ae88bcb9c98
Michael Berkowski <mjb@umn.edu>
2018-05-06 16:06:39 -0500

    Eliminate simple_doi.rb from recursirve include to avoid ruby warnings

--- lib/simple_doi.rb
+++ lib/simple_doi.rb
@@ -1,1 +1,1 @@
-Dir[File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib', '**', '*.rb'))].each { |file| require_relative(file) }
+Dir[File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib', '**', '*.rb'))].reject{|file| file == File.expand_path(__FILE__)}.each { |file| require_relative(file) }
